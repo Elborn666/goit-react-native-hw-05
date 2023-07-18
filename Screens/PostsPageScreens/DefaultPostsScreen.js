7a7454b47593027854f0b3bd32838b7702e3f7bc
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-07-18 18:18:44 +0300

    правка

--- Screens/PostsPageScreens/DefaultPostsScreen.js
+++ Screens/PostsPageScreens/DefaultPostsScreen.js
@@ -11,8 +11,8 @@ const DefaultPostsScreen = ({ route }) => {
       id: 'ksdlflsdnfsldjnfdjfsjdkfn',
       postImg:
         'file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252Fgoit-react-native-hw-05-dbed3e8b-5429-4e6e-a6ba-fefe43283569/Camera/1609f830-6073-4560-8596-6f26f450b3a3.jpg',
-      postName: 'олл',
-      postAddress: 'Ucraine',
+      postName: 'Rest in mountins',
+      postAddress: 'Ukraine',
       postLocation: { latitude: 48.383022, longitude: 31.1828699 },
     },
   ]);
